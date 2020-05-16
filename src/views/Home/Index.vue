379b814dae83fbde9fc9200174863a95856e22cd
Diego Martínez <diegomartinez8995@gmail.com>
2020-05-15 20:23:21 -0600

    refactor: Implements BootstrapVue for project style

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="@/assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-button>Just a Button!</b-button>
   </div>
 </template>
 
