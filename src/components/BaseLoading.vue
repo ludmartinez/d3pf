5531e4deb2f5d63ab824ac4fc659cc4974befe4b
Diego Martínez <diegomartinez8995@gmail.com>
2020-05-17 17:20:27 -0600

    feat: Add BaseLoading global component

---
+++ src/components/BaseLoading.vue
@@ -0,0 +1,78 @@
+<template>
+  <b-overlay
+    :show="show"
+    :variant="variant"
+    :bg-color="bgColor"
+    :opacity="opacity"
+    :blur="blur"
+    :rounded="rounded"
+    :no-wrap="noWrap"
+    :z-index="zIndex"
+    :spinner-type="spinnerType"
+    :spinner-variant="spinnerVariant"
+  >
+    <template v-slot:overlay="props">
+      <div class="text-center">
+        <b-spinner :variant="props.spinnerVariant" :type="props.spinnerType" label="Loading" />
+        <h3 :class="`text-${props.spinnerVariant}`">Loading</h3>
+      </div>
+    </template>
+  </b-overlay>
+</template>
+
+<script>
+export default {
+  name: 'BaseLoading',
+
+  props: {
+    show: Boolean,
+    variant: {
+      type: String,
+      validator: function (value) {
+        return [
+          'primary',
+          'success',
+          'secondary',
+          'warning',
+          'danger',
+          'info',
+          'light',
+          'dark'
+        ]
+          .indexOf(value) !== -1
+      }
+    },
+    bgColor: String,
+    opacity: [Number, String],
+    blur: String,
+    rounded: [Boolean, String],
+    noWrap: {
+      type: Boolean,
+      default: true
+    },
+    zIndex: [String, Number],
+    spinnerType: {
+      type: String,
+      validator: function (value) {
+        return ['border', 'grow'].indexOf(value) !== -1
+      }
+    },
+    spinnerVariant: {
+      type: String,
+      validator: function (value) {
+        return [
+          'primary',
+          'success',
+          'secondary',
+          'warning',
+          'danger',
+          'info',
+          'light',
+          'dark'
+        ]
+          .indexOf(value) !== -1
+      }
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -8,10 +8,14 @@ import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+// Components
+import BaseLoading from '@/components/BaseLoading'
+
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
+Vue.component('BaseLoading', BaseLoading)
 
 new Vue({
   router,
