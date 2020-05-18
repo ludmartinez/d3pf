0ed76b409f5220504c3fece6c22086ee3131f380
Diego Martínez <diegomartinez8995@gmail.com>
2020-05-18 15:58:25 -0600

    feat: Add Navbar and Footer components

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,21 @@
 <template>
   <section class="container">
+    <header-bar></header-bar>
     <router-view></router-view>
+    <footer-bar></footer-bar>
   </section>
 </template>
 
 <script>
+import HeaderBar from '@/components/HeaderBar/Index'
+import FooterBar from '@/components/Footer/Index'
+
 export default {
-  name: 'MainLayout'
+  name: 'MainLayout',
+
+  components: {
+    HeaderBar,
+    FooterBar
+  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,22 +1,42 @@
+// Libraries
 import Vue from 'vue'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import {
+  faGithub,
+  faVuejs,
+  faBootstrap,
+  faBattleNet,
+  faFontAwesome
+} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// Extensions
 import router from './router'
 import store from './store'
-
 // Styles
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '@/assets/css/main.styl'
-
 // Components
+import App from './App.vue' // Main component
 import BaseLoading from '@/components/BaseLoading'
 
+// Configuration
 Vue.config.productionTip = false
 
+library.add(
+  faGithub,
+  faVuejs,
+  faBootstrap,
+  faBattleNet,
+  faFontAwesome,
+  faUserSecret
+)
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
 Vue.component('BaseLoading', BaseLoading)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
