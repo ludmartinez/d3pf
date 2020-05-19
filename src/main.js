260b3950549f31e3d120db6aa8540b4c04280f4e
Diego Martínez <diegomartinez8995@gmail.com>
2020-05-18 18:39:04 -0600

    refactor: Add plugins structure

--- src/main.js
+++ src/main.js
@@ -1,22 +1,11 @@
-// Libraries
 import Vue from 'vue'
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-import {
-  faGithub,
-  faVuejs,
-  faBootstrap,
-  faBattleNet,
-  faFontAwesome
-} from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-// Extensions
+// Plugins
+import '@/plugins/bootstrapVue'
+import '@/plugins/fontAwesome'
+// Vue extensions
 import router from './router'
 import store from './store'
 // Styles
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '@/assets/css/main.styl'
 // Components
 import App from './App.vue' // Main component
@@ -25,18 +14,7 @@ import BaseLoading from '@/components/BaseLoading'
 // Configuration
 Vue.config.productionTip = false
 
-library.add(
-  faGithub,
-  faVuejs,
-  faBootstrap,
-  faBattleNet,
-  faFontAwesome,
-  faUserSecret
-)
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
 Vue.component('BaseLoading', BaseLoading)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
